8ddd73256356d1d2f23a56b58ccaac877189abba
blssd-myxle <michael.sed069@gmail.com>
2021-09-30 05:35:23 +0000

    simple addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *add - adds two integers and returns the result
+ *@a: first number to be added
+ *@b: second number to be added
+ *Return: a + b
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
