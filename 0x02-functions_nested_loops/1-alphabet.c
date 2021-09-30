478f44f359d872e3be3f5390c4aff7e6ace39de2
blssd-myxle <michael.sed069@gmail.com>
2021-09-30 00:18:14 +0000

    Now I know my abc's

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check code for alphabet printing
+ *
+ * Return: 0
+ **/
+
+void print_alphabet(void)
+{
+	int c;
+
+	for ( c = 'a' ; c <= 'z' ; ++c)
+	{
+		_putchar(c);
+
+	}
+	_putchar('\n');
+
+}
