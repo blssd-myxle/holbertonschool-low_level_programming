ca4ab8cfc8d90aa151e9e77f31ab9020983bc214
blssd-myxle <michael.sed069@gmail.com>
2021-09-30 00:44:44 +0000

    fixed unified-diff format

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * main - check code for alphabet printing
+ * main - print_alphabet - printing alphabet using _putchar
  *
  * Return: 0
  **/
 
 void print_alphabet(void)
 {
-	int c;
+	char c;
 
-	for ( c = 'a' ; c <= 'z' ; ++c)
+	for (c = 'a' ; c <= 'z' ; c++)
 	{
 		_putchar(c);
-
 	}
 	_putchar('\n');
-
 }
