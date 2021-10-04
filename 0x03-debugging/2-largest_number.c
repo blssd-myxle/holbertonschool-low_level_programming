dcf621c52c748f7713644469d572f322760e3453
blssd-myxle <michael.sed069@gmail.com>
2021-10-04 14:49:02 -0500

    retry

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,22 +8,22 @@
  * Return: largest number
  */
 
- int largest_number(int a, int b, int c)
- {
-     int largest;
+int largest_number(int a, int b, int c)
+{
+	int largest = a;
 
-     if (a > b && b > c)
-     {
-         largest = a;
-     }
-     else if (b > a && b > c)
-     {
-         largest = b;
-     }
-     else
-     {
-         largest = c;
-     }
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b >= c && b >= a)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-     return (largest);
- }
+	return (largest);
+}
