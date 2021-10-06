1fd4f24a601425bd6f7c168fe55dd2fa8349f0b9
blssd-myxle <michael.sed069@gmail.com>
2021-10-06 03:06:56 -0500

    Pointer to reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *reset_to_98 - resets a pointer to a value of 98
+  *@n: variable for a pointer
+  *Return: nothing
+  */
+
+  void reset_to_98(int *n)
+  {
+    *n = 98;
+  }
