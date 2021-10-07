a92515640a8cf05029814095a783479728dc1ae2
blssd-myxle <michael.sed069@gmail.com>
2021-10-07 03:05:47 -0500

    Printing in base 10.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - printing single digit in base 10 \n
+*
+* Return: 0
+*/
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+	{
+		printf("%d", m);
+	}
+	printf("\n");
+	return (0);
+}
