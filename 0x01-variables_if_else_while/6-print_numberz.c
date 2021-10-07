6940b59d703e844c99a702b8ade81f896ad09a2d
blssd-myxle <michael.sed069@gmail.com>
2021-10-07 03:29:14 -0500

    Print base 10 using putchar.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - print single digit numbers in base 10
+*
+* Return: 0
+*/
+int main(void)
+{
+	int m;
+
+	for (m = 48; m <= 57; m++)
+	{
+		putchar(m);
+	}
+	putchar('n');
+	return (0);
+}
