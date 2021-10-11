349b74cbd0b1667471945cb9a1ba6718311a238c
blssd-myxle <michael.sed069@gmail.com>
2021-10-11 05:30:36 -0500

    Reversing the content of an array.

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* reverse_array - reverses the content of an array
+* @a: pointer to array
+* @n: length of array
+* Return: nothing
+**/
+void reverse_array(int *a, int n)
+{
+	int m, doctor;
+
+	for (m = 0; m < n; m++)
+	{
+		doctor = a[m];
+		a[m] = a[n - 1];
+		a[n - 1] = doctor;
+		n--;
+	}
+}
