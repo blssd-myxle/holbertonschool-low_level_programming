33ef60260e08fdc0d735ca7f2d1e32eb585af3a5
blssd-myxle <michael.sed069@gmail.com>
2021-10-11 03:42:32 -0500

    function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* _strncat - combines two strings
+* @dest: string that is appended
+* @src: the appending string
+* @n: src int
+* Return: dest
+**/
+char *_strncat(char *dest, char *src, int n)
+{
+	int m, x;
+
+	for (x = 0; dest[x] != '\0'; ++x)
+	{
+	}
+	for (m = 0 ; m < n && src[m] != '\0'; m++, x++)
+		dest[x] = src[m];
+	return (dest);
+}
