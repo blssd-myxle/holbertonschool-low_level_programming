8d60bdc818b10840180791350dd6953aaac2a32f
blssd-myxle <michael.sed069@gmail.com>
2021-10-11 04:40:32 -0500

    function that copies a string.

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* _strncpy - copies the string to n
+* @dest: string dest
+* @src: string src
+* @n: int in src
+* Return: dest
+**/
+char *_strncpy(char *dest, char *src, int n)
+{
+    int m = 0;
+
+
+	while (m < n && src[m])
+	{
+		dest[m] = src[m];
+		m++;
+	}
+	while (m < n)
+	{
+		dest[m] = '\0';
+		m++;
+	}
+	return (dest);
+}
