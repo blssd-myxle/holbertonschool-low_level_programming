1be9c9a97ba8763f92002f343ab1e8980f1853b2
blssd-myxle <michael.sed069@gmail.com>
2021-10-13 13:37:46 -0500

    function that fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _memset - fills the first n bytes of memory
+* @s: pointer to the first memory location
+* @b: character to fill memory
+* @n: number of bytes to fills
+* Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int m = 0;
+
+	while (m < n)
+	{
+		s[m] = b;
+		m++;
+	}
+	return (s);
+}
