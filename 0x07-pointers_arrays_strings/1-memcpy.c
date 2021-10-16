2187b110a5bd4844d1b62ca03c1dd752c5bddd91
blssd-myxle <michael.sed069@gmail.com>
2021-10-15 22:08:37 -0500

    Adding 1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _memcpy - copys n bytes of memory
+* @dest: destination string
+* @src: source being copied
+* @n: bytes to copy
+* Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int m;
+
+	for (m = 0; m < n; m++)
+	{
+		dest[m] = src[m];
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,8 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _isupper(int c);
@@ -50,4 +53,4 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 
-#endif /* _HOLBERTON_H_ */
+#endif /* _MAIN_H_ */
