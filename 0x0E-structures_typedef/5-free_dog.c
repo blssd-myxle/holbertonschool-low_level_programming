bc472db420677a884ff7fcc5af064349418e973f
blssd-myxle <michael.sed069@gmail.com>
2021-10-26 22:24:58 -0500

    function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees a dog struct
+ * @d: pointer to struct
+ * 
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
