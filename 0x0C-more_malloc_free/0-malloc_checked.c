033ebb0cc7ffcb4d89c141b27fb7fb5baa6d6a8b
blssd-myxle <michael.sed069@gmail.com>
2021-10-27 00:50:31 -0500

    if don't work come back later

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * malloc_checked - allocates memory using malloc
  * @b: amount of memory to allocate
@@ -8,10 +9,14 @@
  */
 
  void *malloc_checked(unsigned int b)
- {
- 	void *Doctor = malloc(b);
 
- 	if (Doctor == NULL)
+ {
+ 	int *ptr;
+
+ 	ptr = malloc(b);
+
+ 	if(!ptr)
  		exit(98);
- 	return (Doctor);
+
+ 	return (ptr);
  }
