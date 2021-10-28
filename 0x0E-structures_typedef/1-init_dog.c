400a5398f2b5fb2a5e811ecb2393ce333f13e7ca
blssd-myxle <michael.sed069@gmail.com>
2021-10-28 13:37:38 -0500

    fixed space

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,11 +9,11 @@
  * @owner: dog's owner
  * Return: void
  */
- void init_dog(struct dog *d, char *name, float age, char *owner)
- {
- if (!d)
- 	return;
- (*d).name = name;
- (*d).age = age;
- (*d).owner = owner;
- }
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (!d)
+	return;
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
