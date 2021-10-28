f5c2dfc89ca7e6114ed167be72d08bc2adfcc566
blssd-myxle <michael.sed069@gmail.com>
2021-10-28 14:32:12 -0500

    Adding correct file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - locates a char in string
+ *@s: pointer to the memory area to be filled
+ *@c: the char to fill the memory
+ *Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	for (;; s++)
+	{
+		if (*s == c)
+			return (s);
+		if (!*s)
+			return (0);
+	}
+	return (0);
+}
