c31beb2692b4a114d06991f5f7bd48ca47211646
blssd-myxle <michael.sed069@gmail.com>
2021-10-28 14:10:02 -0500

    a program that prints the name of the file that compiled

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints name of the file it was compiled from
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
