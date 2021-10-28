5ff284698acc918be33a192f3a7092149d6953bc
blssd-myxle <michael.sed069@gmail.com>
2021-10-27 22:24:31 -0500

    fix redo push 3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 /**
  * string_nconcat - concatenates two strings, up to n bytes of s2
  *
@@ -11,32 +12,38 @@
  *
  * Return: pointer to new string, or NULL on failure
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+
+ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j;
-	char *dex;
+	char *ptr;
+	unsigned int size1, size2, i, j;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
-
 		s2 = "";
 
-	if (n > strlen(s2))
+	for (size1 = 0; s1[size1] != '\0'; size1++)
+	{}
+	for (size2 = 0; s2[size2] != '\0'; size2++)
+	{}
+	if (n >= size2)
+		n = size2;
 
-		dex = malloc(strlen(s1) + strlen(s2) + 1);
-	else
-		dex = malloc(strlen(s1) + n + 1);
-
-	if (dex == NULL)
-
+	ptr = malloc(sizeof(char) * (size1 + n + 1));
+	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; s1[i]; ++i)
-		dex[i] = s1[i];
 
-	for (j = 0; s2[j] && j < n; ++j, ++i)
-		dex[i] = s2[j];
-	dex[i] = 0;
-	return (dex);
+	for (i = 0; i < size1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		ptr[i] = s2[j];
+		i++;
+	}
+	ptr[i] = '\0';
+
+	return (ptr);
 }
