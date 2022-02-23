2a28147beb0bca930cbf0aef9e0062149eb50982
blssd-myxle <michael.sed069@gmail.com>
2022-02-22 21:47:40 -0600

    redo

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,17 +2,14 @@
 #include <stddef.h>
 
 /**
- * _strpbrk - searches a string for a set of bytes
- * @s: string to search in
- * @accept: bytes to look for
+ * _strpbrk - searches string for any set of bytes
+ *@s: pointer
+ *@accept: string
  *
- * @accept: string
+ * Return: char pointer s
  */
-
-
-
- char *_strpbrk(char *s, char *accept)
- {
+char *_strpbrk(char *s, char *accept)
+{
 	char *str = NULL;
 
 	if (s == NULL || accept == NULL)
@@ -33,4 +30,4 @@
 		s++;
 	}
 	return (NULL);
- }
+}
