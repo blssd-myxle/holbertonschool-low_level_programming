69233e3eb16706834946fbad9dc6b4e7eb5406a4
blssd-myxle <michael.sed069@gmail.com>
2022-02-24 17:21:13 -0600

    push it 2

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,12 +7,12 @@
  * Return: the factorial of n
  */
 
- int factorial(int n)
- {
- 	if (n == 0)
- 		return (1);
- 	else if (n < 0)
- 		return (-1);
- 	else
- 		return (n * factorial(n - 1));
- }
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (n * factorial(n - 1));
+}
