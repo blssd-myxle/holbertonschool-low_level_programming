d371bc412c7109ba8df19739a654b395f1cae06a
blssd-myxle <michael.sed069@gmail.com>
2022-02-24 18:47:11 -0600

    push a

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,9 +9,9 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
-{
+ int main(int argc, __attribute__((unused)) char  *argv[])
+ {
 	if (argc > 0)
-		printf("%s\n", argv[0]);
+		printf("%d\n", argc - 1);
 	return (0);
-}
+ }
