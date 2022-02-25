08e6a9081af16c50adfc10076a670878015a6dcb
blssd-myxle <michael.sed069@gmail.com>
2022-02-24 19:33:48 -0600

    push redo

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,22 +9,29 @@
  *
  *Return: nil
  */
-int main(int argc, char *argv[])
-{
-	int i, j, sum = 0;
 
-	for (i = 1; i < argc; ++i)
-	{
-		for (j = 0; argv[i][j]; ++j)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		sum += atoi(argv[i]);
-	}
-	printf("%d\n", sum);
-	return (0);
-}
+ int main(int argc, char  *argv[])
+ {
+ 	int i, j, sum = 0;
+
+ 	if (argc == 1)
+ 	{/*if no arguments */
+ 		printf("0\n");
+ 		return (0);
+ 	}
+ 	for (i = 1; i < argc; i++)
+ 	{
+ 		for (j = 0; argv[i][j]; j++)
+ 		{
+
+ 			if (!isdigit(argv[i][j]))
+ 			{
+ 				printf("Error\n");
+ 				return (1);
+ 			}
+ 		}
+ 		sum = sum + atoi(argv[i]);
+ 	}
+ 	printf("%d\n", sum);
+ 	return (0);
+ }
