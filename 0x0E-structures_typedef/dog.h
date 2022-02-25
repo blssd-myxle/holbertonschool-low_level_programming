82992c72093d88f61b9bd1b9d80cd6740ded522b
blssd-myxle <michael.sed069@gmail.com>
2022-02-24 22:41:53 -0600

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,27 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct dog - structure for a dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * struct dog - struct about a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
  *
- * Description: data structure for dog
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /* _DOG_H_ */
+
+#endif
