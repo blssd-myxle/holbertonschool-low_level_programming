19812b4273ca8ad6f2232c9f833ac5b7c919104c
blssd-myxle <michael.sed069@gmail.com>
2022-02-24 20:04:08 -0600

    fix ws to make betty :)

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,37 +1,30 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-/**
- * main - add agrs on command line
- *@argc: argument count
- *@argv: agurment array vector
- *
- *Return: nil
- */
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <ctype.h>
+ /**
+  * main - add agrs on command line
+  *@argc: argument count
+  *@argv: agurment array vector
+  *
+  *Return: 0
+  */
+ int main(int argc, char *argv[])
+ {
+	int a, b, sum = 0;
 
- int main(int argc, char  *argv[])
-{
-        int i, j, sum = 0;
-
-        if (argc == 1)
-    {
-        printf("0\n");
-            return (0);
-    }
-        for (i = 1; i < argc; i++)
-        {
-            for (j = 0; argv[i][j]; j++)
-        {
-
-            if (!isdigit(argv[i][j]))
-            {
-                printf("Error\n");
-            return (1);
-            }
-        }
-            sum = sum + atoi(argv[i]);
-        }
-    printf("%d\n", sum);
-    return (0);
+	for (a = 1; a < argc; ++a)
+	{
+		for (b = 0; argv[a][b] != '\0'; ++b)
+		{
+			if (!isdigit(argv[a][b]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[a]);
+	}
+	 printf("%d\n", sum);
+	return (0);
  }
