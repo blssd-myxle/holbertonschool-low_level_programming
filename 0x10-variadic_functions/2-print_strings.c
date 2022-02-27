51d1982d14438f98aab334cc025aacea03e07c7e
blssd-myxle <michael.sed069@gmail.com>
2022-02-26 20:43:16 -0600

    fix to make betty :) 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,22 +9,22 @@
  *Return: nil
  */
 
- void print_strings(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
- 	unsigned int i;
- 	char *s;
- 	va_list str;
+	unsigned int i;
+	char *s;
+	va_list str;
 
- 	va_start(str, n);
- 	for (i = 0; i < n; i++)
- 	{
- 		s = va_arg(str, char *);
- 		if (s == NULL)
- 			s = "(nil)";
- 		printf("%s", s);
- 		if (separator != NULL && i < (n - 1))
- 			printf("%s", separator);
- 	}
- 	printf("\n");
+	va_start(str, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(str, char *);
+		if (s == NULL)
+			s = "(nil)";
+		printf("%s", s);
+		if (separator != NULL && i < (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
 	va_end(str);
 }
